a096f44da2033b7824ca929b0520fe76e401d7b6
zbahniuk <zakhar.bahniuk@cpexterne.org>
2025-03-11 07:18:42 +0000

    init commit

---
+++ tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, Subjects, TicketUpdatedEvent } from '@zbahntickets/common';
+
+export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+}
